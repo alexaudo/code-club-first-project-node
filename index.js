f068aff22770755ba44f7885ea7f54e3e90ddca9
alexaudo <alexa.anu19@gmail.com>
2023-05-10 16:24:19 -0500

    finished my first project node

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const uuid = require('uuid')
-
 const port = 3000
-
 const app = express()
 app.use(express.json())
-
 
 const users = []
 
@@ -31,18 +28,22 @@ app.get('/users', (request, response) => {
 })
 
 app.post('/users', (request, response) => {
+
     const { name, age} = request.body
     
     const user = { id:uuid.v4(), name, age }
 
     users.push(user)
+
     return response.status(201).json(users)
 })
 
 app.put('/users/:id', checkUserId, (request, response) => {
    
     const { name, age } = request.body
+    
     const index = request.userIndex
+    
     const id = request.userId
 
     const updateUser = { id, name, age }
